e81368f1b95e5d6e1e8acc9f8383fb9eba33702a
danystefanescu <115418235+danystefanescu@users.noreply.github.com>
2022-12-06 18:29:39 +0100

    Added 2 import and 2 React components
    Added import cards and footer + cards component and footer component

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Navbar from "./navbar.js";
 import Jumbotron from "./jumbotron.js";
+import Footer from "./footer.jsx";
+import Cards from "./cards.jsx";
 
 
 //include images into your bundle
@@ -23,7 +25,9 @@ const Home = () => {
 					descriptionJumbotron="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 					buttonLabelJumbotron="Call to action!"
 				/>
+				<Cards />
 			</div>
+			<Footer />
 		</>
 	);
 };
