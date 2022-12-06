035b38c68f358da16bb9c63c1c1831d802997691
AlexPinaBr <alexpinabraojos@gmail.com>
2022-12-06 16:56:51 +0000

    Jumbotron is done

---
+++ src/js/component/jumbotron.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
+
+const Jumbotron = (props) => {
+	return (
+        <div class="container-fluid py-5 bg-light mt-5">
+        <h1 class="display-5 fw-bold">{props.titleJumbotron}</h1>
+        <p class="col-md-8 fs-4">{props.descriptionJumbotron}</p>
+        <button class="btn btn-primary btn-lg" type="button">{props.buttonLabelJumbotron}</button>
+      </div>
+	);
+};
+
+Jumbotron.propTypes = {
+	titleJumbotron: PropTypes.string,
+	descriptionJumbotron: PropTypes.string,
+	buttonLabelJumbotron: PropTypes.string
+};
+
+export default Jumbotron;
