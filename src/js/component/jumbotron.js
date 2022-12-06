3c114521e3c114770fb3764d354d2b2c67a2a054
danystefanescu <115418235+danystefanescu@users.noreply.github.com>
2022-12-06 18:49:45 +0100

    Added margin bottom 5 at div parent class

--- src/js/component/jumbotron.js
+++ src/js/component/jumbotron.js
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 
 const Jumbotron = (props) => {
 	return (
-        <div class="container-fluid py-5 bg-light mt-5">
+        <div class="container-fluid py-5 bg-light mt-5 mb-5">
         <h1 class="display-5 fw-bold">{props.titleJumbotron}</h1>
         <p class="col-md-8 fs-4">{props.descriptionJumbotron}</p>
         <button class="btn btn-primary btn-lg" type="button">{props.buttonLabelJumbotron}</button>
